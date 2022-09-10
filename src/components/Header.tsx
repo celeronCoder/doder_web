0e7d94f1eecf05169e7524e5b7711cef196fa013
Bhardwaj <khushalbhardwaj0111@gmail.com>
2022-09-10 13:18:35 +0530

    redid the navbar with search and avatar (with menu)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,42 @@
 export const Header: React.FC = () => {
   return (
-    <div className="sw-full h-20 border-b-[1px] border-primary flex items-center justify-center">
-      <h1 className="logo text-2xl font-bold">Doder</h1>
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl font-heading">Doder</a>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="https://placeimg.com/80/80/people" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };

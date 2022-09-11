2bb8e293ba848db864f373780ea54341953a68a8
Bhardwaj <khushalbhardwaj0111@gmail.com>
2022-09-11 10:05:40 +0530

    added list and create for todos

---
+++ src/server/router/todo.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { createProtectedRouter } from "./context";
+import { Priority } from "@prisma/client";
+
+export const todoRouter = createProtectedRouter()
+  .query("getAll", {
+    async resolve({ ctx }) {
+      return await ctx.prisma.todo.findMany({
+        where: { userId: ctx.session.user.id },
+      });
+    },
+  })
+  .query("getOne", {
+    input: z.object({
+      id: z.string().cuid(),
+    }),
+    async resolve({ ctx, input }) {
+      return await ctx.prisma.todo.findFirst({
+        where: { userId: ctx.session.user.id, id: input.id },
+      });
+    },
+  })
+  .mutation("create", {
+    input: z.object({
+      title: z.string(),
+      description: z.string(),
+      dueDate: z.date().optional(),
+      priority: z.nativeEnum(Priority),
+    }),
+    async resolve({ ctx, input }) {
+      return await ctx.prisma.todo.create({
+        data: {
+          userId: ctx.session.user.id,
+          title: input.title,
+          description: input.description,
+          dueDate: input.dueDate,
+          priority: input.priority,
+        },
+        select: {
+          id: true,
+        },
+      });
+    },
+  });

a010dcfe9d5c4e23c3454964171f5ca4cd6871db
Bhardwaj <khushalbhardwaj0111@gmail.com>
2022-09-17 13:05:50 +0530

    replaced raw svg with tabler-icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from "@mantine/core";
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useEffect } from "react";
@@ -23,37 +24,7 @@ export const Header: React.FC = () => {
             className="input input-bordered"
           />
         </div>
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="avatar">
-              <div className="w-10 rounded-full">
-                <Image
-                  src={session.data?.user?.image as string}
-                  alt={session.data?.user?.name!}
-                  layout="fill"
-                  className="rounded-full w-10"
-                />
-              </div>
-            </div>
-          </label>
-          <ul
-            tabIndex={0}
-            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li onClick={() => signOut()}>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
+        <Avatar src={session.data?.user?.image} />
       </div>
     </div>
   );
